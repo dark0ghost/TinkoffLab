6b47300b6463a05a899c1bc518db1883bb39c7c7
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-09-04 02:26:44 +0300

    make ui

--- app/src/main/java/org/dark0ghost/tinkoff_app_test/menu_details/MainScreen.kt
+++ app/src/main/java/org/dark0ghost/tinkoff_app_test/menu_details/MainScreen.kt
@@ -1,6 +1,7 @@
 package org.dark0ghost.tinkoff_app_test.menu_details
 
 import android.content.Context
+import android.graphics.Color
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -8,9 +9,12 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.ui.graphics.Color.Companion.Yellow
 import androidx.ui.tooling.preview.Preview
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -32,24 +36,29 @@ fun InetError() {
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun renderPage(){
+fun RenderPage(){
     val pagerState = rememberPagerState(pageCount = 3)
 
     TabRow(
-        // Our selected tab is our current page
         selectedTabIndex = pagerState.currentPage,
-        // Override the indicator, using the provided pagerTabIndicatorOffset modifier
+        backgroundColor = White,
         indicator = { tabPositions ->
             TabRowDefaults.Indicator(
                 Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
             )
         }
+
     ) {
         // Add tabs for all of our pages
 
         Tab(
             text = { Text("12") },
             selected = pagerState.currentPage == 1,
+            onClick = { /* TODO */ },
+        )
+        Tab(
+            text = { Text("13") },
+            selected = pagerState.currentPage == 2,
             onClick = { /* TODO */ },
         )
 
@@ -60,25 +69,32 @@ fun renderPage(){
     }
 }
 
+@Composable
+fun Profile(){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(40.dp))
+        Text("By dark0ghost", fontSize = 30.sp)
+        //!isNetworkAvailable(this as Context)) InetError()
+    }
+}
 
 @OptIn(ExperimentalPagerApi::class)
 @Preview
 @Composable
 fun MainScreen() {
-
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier.padding(all = 8.dp)) {
-            Image(
-                painter = painterResource(R.drawable.profile),
-                contentDescription = "Contact profile picture",
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-            )
-
-            Spacer(modifier = Modifier.width(40.dp))
-            Text("By dark0ghost", fontSize = 30.sp)
-            //!isNetworkAvailable(this as Context)) InetError()
+    Surface {
+        Column {
+            Profile()
+            RenderPage()
         }
     }
+
 }
